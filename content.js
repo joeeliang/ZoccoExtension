00442f2cd00ae89b3e0a7969e520431e9087950a
joeeliang <103452146+joeeliang@users.noreply.github.com>
2023-07-15 16:06:16 -0700

    A tags are ignored

--- content.js
+++ content.js
@@ -1,16 +1,25 @@
 // Function to bold the first letter of each word in all <p> elements
-function boldFirstLetters(text, boldedAmount) {
+function boldFirstLettersIgnoretag(text, boldedAmount) {
     // Split the text into an array of words
     const words = text.split(' ');
+  
+    // Replace each matched tag and its content with modified content
 
     // Iterate through each word and bold the first letter
     const modifiedWords = words.map(word => {
-        if (word.length > boldedAmount) {
-        const firstChar = word.slice(0,boldedAmount);
-        const restOfWord = word.slice(boldedAmount);
-        return `<b>${firstChar}</b>${restOfWord}`;
+        console.log(word);
+        console.log(typeof word);
+        let tags = '<>=\'\"';
+
+        if (tags.split("").some(char => word.includes(char))){
+            console.log("This has equal tag! Skip!")
+            return word;
+        } else if(word.length > boldedAmount) {
+            const firstChar = word.slice(0,boldedAmount);
+            const restOfWord = word.slice(boldedAmount);
+            return `<b>${firstChar}</b>${restOfWord}`;
         } else {
-        return word;
+            return `<b>${word}</b>`;
         }
     });
 
@@ -19,6 +28,38 @@ function boldFirstLetters(text, boldedAmount) {
 
     return modifiedText;
 }
+
+function boldFirstLetters(text, boldedAmount) {
+    // Match opening and closing HTML tags and their content
+    const regex = /(<\/?[a-zA-Z]+\b[^>]*>)([^<>]+)/g;
+  
+    // Replace each matched tag and its content with modified content
+    const modifiedText = text.replace(regex, (match, tag, content) => {
+        if (tag.includes('<a') && tag.includes('href=')) {
+            console.log("returned, a tag found");
+            console.log(match)
+            return match; // Skip modification for <a> tags with href attribute
+        }
+  
+    const modifiedContent = content.replace(/\b(\w)/g, (wordMatch) => {
+        console.log("Below is the match thingy");
+        console.log(wordMatch);
+        const firstChar = wordMatch.slice(0, boldedAmount);
+        const restOfWord = wordMatch.slice(boldedAmount);
+        console.log("returned, modified")
+        console.log(`<b>${firstChar}</b>${restOfWord}`)
+        return `<b>${firstChar}</b>${restOfWord}`;
+    });
+        console.log("returned content modified")
+        console.log(tag + modifiedContent);
+        return tag + modifiedContent;
+    });
+
+    console.log("All returns passed, this is the end")
+    console.log(modifiedText);
+    return modifiedText;
+}
+  
 
 function removeBoldTags(str) {
     const regex = /<b>|<\/b>/gi; // Regular expression to match <b> and </b> tags
@@ -33,7 +74,7 @@ function boldFirstLettersInParagraphs(boldedAmount) {
     paragraphs.forEach(paragraph => {
         const originalText = paragraph.innerHTML;
         const boldlessText = removeBoldTags(originalText);
-        const modifiedText = boldFirstLetters(boldlessText,boldedAmount);
+        const modifiedText = boldFirstLettersIgnoretag(boldlessText,boldedAmount);
         paragraph.innerHTML = modifiedText;
     });
 }
